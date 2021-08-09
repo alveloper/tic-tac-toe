61599ce6ac731b65ae1348cbb742bd2dac350d18
alveloper <alveloper@gmail.com>
2021-08-10 02:05:57 +0900

    17. Running our Scripts Before Committing to Git Using Husky & Lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
-    "prettier/@typescript-eslint",
-    "prettier/react",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
