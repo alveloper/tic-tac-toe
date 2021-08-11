03a87b2f58d140e9a368ac0dc7c56278b8dfba38
alveloper <alveloper@gmail.com>
2021-08-11 11:18:30 +0900

    22. Creating a Component for Bootstrapping our App

---
+++ src/components/app-bootstrap/app-bootstrap.tsx
@@ -0,0 +1,22 @@
+import React, { ReactElement, ReactNode } from "react";
+import {
+  useFonts,
+  DeliusUnicase_400Regular,
+  DeliusUnicase_700Bold,
+} from "@expo-google-fonts/delius-unicase";
+import { Home } from "@screens";
+
+type AddBootstrapProps = {
+  children: ReactNode;
+};
+
+// loads our fonts
+export default function AppBootstrap({
+  children,
+}: AddBootstrapProps): ReactElement {
+  const [fontLoaded] = useFonts({
+    DeliusUnicase_400Regular,
+    DeliusUnicase_700Bold,
+  });
+  return fontLoaded ? <>{children}</> : <Home />;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,17 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import { StyleSheet, View } from "react-native";
-import {
-  useFonts,
-  DeliusUnicase_400Regular,
-  DeliusUnicase_700Bold,
-} from "@expo-google-fonts/delius-unicase";
-import { Text } from "@Components";
+import { Text, AppBootstrap } from "@Components";
 
-export default function App() {
-  const [fontLoaded] = useFonts({
-    DeliusUnicase_400Regular,
-    DeliusUnicase_700Bold,
-  });
-  if (!fontLoaded) return null;
+export default function App(): ReactElement {
+  // if (!fontLoaded) return null;
   return (
-    <View style={styles.container}>
-      <Text onPress={() => alert(true)} style={{ fontSize: 25 }}>
-        Hello world <Text weight="400">and hello to you!</Text>
-      </Text>
-    </View>
+    <AppBootstrap>
+      <View style={styles.container}>
+        <Text onPress={() => alert(true)} style={{ fontSize: 25 }}>
+          Hello world <Text weight="400">and hello to you!</Text>
+        </Text>
+      </View>
+    </AppBootstrap>
   );
 }
 
